5b9c1e31d0065ea975d2c66a7fdf0e3e87e71fb7
yogapriya <yogapriya.s98@gmail.com>
2018-05-29 17:49:28 +0530

    DataType
    Operator

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String s[]) {
+		byte one=100;
+		byte two=50;
+		int three = one + two;
+		System.out.println(three);
+
+	}
+}
---
+++ src/com/basic/OperatorExample.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class OperatorExample {
+	public static void main(String s[]) {
+		int count=10;
+		count++;
+		--count;
+		int negCount =-20;
+		boolean response=false;
+	    System.out.println(!response);
+	    
+		System.out.println( count < 20);
+		System.out.println( count > 20);
+		
+		System.out.println( count >= 10);
+		System.out.println( count <= 10);
+		System.out.println( 4 & 2);
+		
+		System.out.println( 6 | 4);
+		
+		System.out.println( 4 ^ 2);
+		System.out.println((5*2) + 5 - -3* (4*2) + 5 + "better" + (5*2) + 2);
+		}
+
+}
