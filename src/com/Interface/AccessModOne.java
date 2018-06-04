f8d0420751bf810e934d13231faf2f071d12ce66
yogapriya <yogapriya.s98@gmail.com>
2018-06-04 12:26:25 +0530

    more on -AccessSpecifier

---
+++ src/com/Interface/AccessModOne.java
@@ -0,0 +1,7 @@
+package com.Interface;
+
+public class AccessModOne {
+	protected int count = 100;  //protected can use-difference package sub class
+	
+
+}
---
+++ src/com/basic/AccessModOneSub.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+import com.Interface.AccessModOne;
+
+public class AccessModOneSub extends AccessModOne{
+	 public AccessModOneSub() {
+		 count++;   
+		 
+	 }
+
+}
