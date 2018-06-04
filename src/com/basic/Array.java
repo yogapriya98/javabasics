14d6838b820a48a547ccef6da04b0e312c812bfe
yogapriya <yogapriya.s98@gmail.com>
2018-06-04 12:24:57 +0530

    Array-basicConcept

---
+++ src/com/basic/Array.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class Array {
+	public static void main(String args[]) {
+		int[] squares = new int[4];
+	    for (int i = 0; i < squares.length; i++) {
+			System.out.println(squares[i] = i*2);
+		}
+	    // kind of array creation
+	   int[] number = {2,1,5,6,4,7};
+	   
+	    Car car[] = {new Car(),new Car(),new Car()};
+	    for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	    
+	    // java is a row major array
+	    int[][] arr = new int[5][];
+     }
+}
