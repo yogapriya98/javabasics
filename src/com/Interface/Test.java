407a3b0317b8e401439a50efe9104ffa5bed059e
yogapriya <yogapriya.s98@gmail.com>
2018-06-04 12:31:06 +0530

    More on -StaticImporter

--- src/com/Interface/Test.java
+++ src/com/Interface/Test.java
@@ -1,9 +1,15 @@
 package com.Interface;
-
+import static java.lang.Math.PI;     // static importer(variables)
+import static java.lang.Math.abs;    // static importer(methods)
 public class Test {
 	public static void main(String args[]) {
 		Travel travelOne = new Car();
 		Travel travelTwo = new Dog();
 		System.out.println(travelOne instanceof Car);
+		 Travel travelThree = new Rabbit();  //behave like travel
+		 Owner owner =(Rabbit)travelThree;     //behave like owner(TypeCast)
+		 
+		 double d = PI;//(or)double d = Math.PI;
+		 abs(5);
 }
 }
