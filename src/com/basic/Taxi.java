936dd04c2137d2aab1396e48627d0829aceb8f88
yogapriya <yogapriya.s98@gmail.com>
2018-06-05 15:05:40 +0530

    more on:
    	# Constructorchaining
    	# super()
    	#this()
    	#OverrideMethod
    	#ObjectSubtitution

--- src/com/basic/Taxi.java
+++ src/com/basic/Taxi.java
@@ -1,18 +1,33 @@
 package com.basic;
 
 public class  Taxi extends Car{
+	int i=10;
+	//Constructor chaining
+	
 	public Taxi() {
 	//	super();
-	//System.out.println("taxi brake call");
-	}
-	public void brake() {
-		System.out.println("taxi brake call");
+	System.out.println("taxi brake call");
 	}
 	public Taxi(int i) {
-		//super();
+		this.brake();
 		System.out.println("arg cons called");
+	}
+	// Override method 
+	   @Override                        //if not override superclass brake method is called
+	     public void brake() {
+		 System.out.println("taxi brake");
 	}
 public static void main(String args[]) {
 	Taxi taxi = new Taxi(10);
+	taxi.brake();
+	
+	//ObjectSubtitution
+	
+	Car car = new Taxi();
+	car.brake();
+	System.out.println(car.i); //within a package -no modifier can access
+	// instanceof
+	
+	System.out.println(car instanceof Taxi);
 }
 }
