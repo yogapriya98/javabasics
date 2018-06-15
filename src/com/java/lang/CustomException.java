289f23ce55e3090a99623dd55e8c812476cc7ea3
yogapriya <yogapriya.s98@gmail.com>
2018-06-15 18:39:30 +0530

    more on -throws,throw exception

---
+++ src/com/java/lang/CustomException.java
@@ -0,0 +1,8 @@
+package com.java.lang;
+
+public class CustomException extends Exception{
+        public CustomException(String msg) {
+        super (msg);
+        }
+
+}
--- src/com/java/lang/ExceptionExample.java
+++ src/com/java/lang/ExceptionExample.java
@@ -33,5 +33,38 @@ public class ExceptionExample {
 	}
   public static void main(String[] args) {
 	ExceptionExample ee = new ExceptionExample();
+	try {
+		ee.getCustomer();
+	}   catch (IOException e) {       //MalformedURLException subtype of IOException so,IOException occurs in catch 
+		e.printStackTrace();
+	}
+	try {
+		ee.getCustomer2();
+	} catch (CustomException e) {
+		e.printStackTrace();
+	}
+  }
+  public void getCustomer() throws MalformedURLException,IOException {
+	 // ExceptionExample ee = null;
+	 // ee.hashCode();
+	  URL url = new URL("http://www.google.com");
+  }
+  public void getCustomer1() throws MalformedURLException {   //throws
+	/*  try {
+		  ExceptionExample ee = null;
+		  ee.hashCode();
+         } catch (Exception e) {
+			  throw new MalformedURLException();              //throw
+		  } */
+	  int count = 100;
+	  if(count < 50) {
+		  throw new MalformedURLException();
+     }
+  }
+  public void getCustomer2() throws CustomException{
+	int count = 100;
+	if(count<50) {
+		throw new CustomException("lesser than 100");	
+	}
 }
 }
