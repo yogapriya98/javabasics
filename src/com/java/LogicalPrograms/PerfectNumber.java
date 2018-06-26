527e988819dfd6e365b0e2c3110c487df9711e36
yogapriya <yogapriya.s98@gmail.com>
2018-06-26 12:58:28 +0530

    Program to find  PerfectNumber

---
+++ src/com/java/LogicalPrograms/PerfectNumber.java
@@ -0,0 +1,21 @@
+package com.java.LogicalPrograms;
+
+public class PerfectNumber {
+
+	public boolean isperfect(int number) {
+		int i,sum=0;
+		for(i=1; i<number; i++) {
+			if(number%2 == 0) {
+				sum+= number;
+			}
+			
+		}	
+		return((sum==number) ? true :false);
+	}
+	public static void main(String s[]) {
+		PerfectNumber a1 = new PerfectNumber();
+		a1.isperfect(6);
+		System.out.println(a1.isperfect(6));
+	}
+}
+
