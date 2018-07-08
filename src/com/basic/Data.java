3f2cdede003a1cd815bd34d97391391200453e86
yogapriya <yogapriya.s98@gmail.com>
2018-07-08 11:32:10 +0530

    A program to find Stable number

---
+++ src/com/basic/Data.java
@@ -0,0 +1,25 @@
+package com.basic;
+import java.util.Scanner;
+public class Data { 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter n to compute the nth prime number: ");
+ 
+    int nth = sc.nextInt();
+    int num, count, i;
+    num=1;
+    count=0;
+    while (count < nth){
+      num=num+1;
+      for (i = 2; i <= num; i++){
+        if (num % i == 0) {
+          break;
+        }
+      }
+      if ( i == num){
+        count = count+1;
+      }
+    }
+    System.out.println("Value of nth prime: " + num);
+  }
+}
